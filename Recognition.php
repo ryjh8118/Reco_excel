68be4488f9944c06dadd43cbf1ddb1ba77ec4e61
Renguin0829 <ryjh8118@gmail.com>
2017-12-22 16:20:11 +0800

    12/22_將掃到的資料儲存至Excel

---
+++ Recognition.php
@@ -0,0 +1,24 @@
+<?php
+
+$getDate= date("Y_md"); //取得日期
+
+//偵錯用
+print 'age:'.$_GET['age'].'<br>';
+print 'gender:'.$_GET['gender'];
+
+if ( !is_file("Reco/".$getDate."_Recognition.xls")):
+		$fp = fopen("Reco/".$getDate."_Recognition.xls", "w"); //直接複寫
+		fputs($fp,$getDate."\n");
+	endif;
+	
+	$fp = fopen("Reco/".$getDate."_Recognition.xls", "a"); //若有接寫
+
+
+fputs($fp,"age\t".$_GET['age']."\n");
+fputs($fp,"gender\t".$_GET['gender']."\n");
+fputs($fp,"\n");
+
+
+fclose($fp);
+
+?>
