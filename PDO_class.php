875eae49e051da10ee377e358ee1d5062c05e444
Renguin0829 <ryjh8118@gmail.com>
2017-12-28 17:34:42 +0800

    1228 PDO物件導向化 資料庫自動新增

---
+++ PDO_class.php
@@ -0,0 +1,46 @@
+<?php
+///////////////////////////////////////////////////
+class PDOsetting{
+	public $dbname = 'nalaning1590';
+	public $host = 'localhost';
+	public $port = '3306';
+	public $username = 'root';
+	public $userpwd = '';
+
+	function __construct(){
+		$this->sql_setting();
+		$this->sql_connect();
+	}
+
+	function sql_setting(){
+		return 'mysql:dbname='.$this->dbname.';host='.$this->host.';port='.$this->port;	
+	}
+
+	function sql_connect(){
+		try {
+		   $this->sql_link();
+		   echo "成功建立MySQL伺服器連接和開啟資料庫<br>-----------------------------------<br>"; 
+
+		} catch (PDOException $e) {
+		   echo "連接失敗: " . $e->getMessage();
+		}
+	}
+
+	function sql_encode(){
+		return array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8");
+	}
+
+	function sql_link(){
+		$link = new PDO($this->sql_setting(),
+		           $this->username,
+		           $this->userpwd,
+		           $this->sql_encode()   //沒輸入無法秀出中文
+		           );  //連上 資料庫               並做編碼設定
+		return $link;
+	}
+		
+
+
+}
+
+?>
--- Recognition.php
+++ Recognition.php
@@ -1,18 +1,47 @@
 <?php
-include("PDO.php");
-$getDate= date("Y_md"); //取得日期
+// 本機端
+include("PDO_class.php");
+$mysqldb = new PDOsetting;
 
+
+//$getDate= date("Y-m-d h:i:s"); //取得日期
 //偵錯用
-print 'age:'.$_GET['age'].'<br>';
-print 'gender:'.$_GET['gender'];
+	print 'age:'.$_GET['age'].'<br>';
+	print 'gender:'.$_GET['gender'];
+/////////////
+	$humanage = $_GET['age'];
+		if($_GET['gender']=='male')
+			$humangender = 1;
+		else
+			$humangender = 0;
+//////////////
+
+	///新增資料////////////////////////
+	try {
+		$sql = "INSERT INTO facereco_list (age,gender) VALUES ($humanage,$humangender)";
+		$mysqldb->sql_link()->query($sql);
+		echo '<br><font color="#4169E1">新增_成功</font>';
+	}
+	catch(Exception $e){
+		echo '<br><font color="#FF0000">新增_失敗</font> ';
+	}
+	///////////////////////////////////	
+
+///修改資料////////////////////////
+/*
+try {
+	//$sql = "SELECT NOW();";
+	$sql = "UPDATE facereco_list SET id=0, age=20 WHERE id=1";
+	$link->query($sql);
+	echo '<br>修改成功';
+} catch(Exception $e){
+	echo '<br>修改失敗';
+}
+///////////////////////////////////	*/
 
 
 
-
-
-
-
-
+/////EXCEL//////////////////////////////////
 /*
 if ( !is_file("Record/".$getDate."_Recognition.xls")):
 		$fp = fopen("Record/".$getDate."_Recognition.xls", "w"); //直接複寫
@@ -29,4 +58,5 @@ fputs($fp,"\n");
 
 fclose($fp);
 */
+//////////////////////////////////////////////////
 ?>
