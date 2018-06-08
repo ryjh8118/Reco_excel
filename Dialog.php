1ebb3618da29c36221c3a0c578708e45c0db8266
Renguin0829 <ryjh8118@gmail.com>
2018-06-08 17:38:51 +0800

    0608很順的時候 在微調

---
+++ Dialog.php
@@ -0,0 +1,21 @@
+<head>
+	<style>
+		body{
+			/*height:100%;*/
+			/*width:100%;*/
+		margin:0px;
+		padding:0px;
+		/*background-color: #FFBB66;*/
+		background-repeat:no-repeat;
+		/*background-position:center;*/
+		background-size:100% 100% ;　//設定背景圖片的填滿方式
+		}    
+
+</style>
+</head>
+
+<body>
+	<?php   // 本機端
+	echo "<div> <body background=\"img\pepper.jpg\"></div>";
+	?>
+</body>
